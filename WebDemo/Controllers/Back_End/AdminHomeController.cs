320535b80169dfdef32919338e9e60895008e2bb
Do-Huy-Hoang <92659956+Do-Huy-Hoang@users.noreply.github.com>
2022-12-09 15:47:31 +0700

    update_4

---
+++ WebDemo/Controllers/Back_End/AdminHomeController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDemo.Controllers.Back_End
+{
+    public class AdminHomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View("Views/Back-end/AdminHome/index.cshtml");
+        }
+    }
+}
