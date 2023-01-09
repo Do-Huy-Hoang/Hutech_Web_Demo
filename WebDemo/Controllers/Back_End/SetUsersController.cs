d9af7644ab33641e42d189c1a6da95c7c68e49db
ITPKanon <vinh817785@gmail.com>
2023-01-09 17:29:25 +0700

    vinh_9/1
    setuser

---
+++ WebDemo/Controllers/Back_End/SetUsersController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace WebDemo.Controllers.Back_End
+{
+    public class SetUsersController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
